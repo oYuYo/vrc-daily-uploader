281bd2cba2c2f597ba9549e2a9a0e2812c57d398
oYuYo <year0120@gmail.com>
2023-11-30 22:45:46 +0900

    縦向きの写真を横向きへ, Mutexのタイミングを誤っていたので削除

--- main.go
+++ main.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/jpeg"
 	"io"
 	"log"
 	"math/rand"
@@ -104,6 +108,7 @@ func SavePhoto(conf flickrapi.Config, photoList flickrapi.Photo, num int) error 
 	if err != nil {
 		return err
 	}
+
 	//fmt.Println(u)
 
 	resp, err := http.Get(u)
@@ -111,17 +116,59 @@ func SavePhoto(conf flickrapi.Config, photoList flickrapi.Photo, num int) error 
 		return err
 	}
 	defer resp.Body.Close()
+
+	img, _, err := image.Decode(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	var rotatedImg image.Image
+	width, height := img.Bounds().Dx(), img.Bounds().Dy()
+	if isHorizontal(width, height) {
+		rotatedImg = img
+	} else {
+		rotatedImg = rotateToHorizonal(img)
+	}
+
 	file, err := os.Create("./image" + strconv.Itoa(num) + ".jpg")
 	if err != nil {
 		return err
 	}
 	defer file.Close()
 
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
+	if err := saveImageToFile(rotatedImg, file); err != nil {
 		return err
 	}
+
 	return nil
+}
+
+func isHorizontal(width int, height int) bool {
+	flg := false
+	if width > height {
+		flg = true
+	}
+	return flg
+}
+
+// 縦向きの写真を横向きへ, 背景は白で塗りつぶす(w:h = 10:7 で表示しているため)
+func rotateToHorizonal(img image.Image) image.Image {
+	newWidth := int(float64(img.Bounds().Dy()) / 0.7)
+	newHeight := img.Bounds().Dy()
+	resizedImg := image.NewRGBA(image.Rect(0, 0, newWidth, newHeight))
+
+	draw.Draw(resizedImg, resizedImg.Bounds(), &image.Uniform{color.White}, image.Point{}, draw.Over)
+
+	centerX := (newWidth - img.Bounds().Dx()) / 2
+	centerY := (newHeight - img.Bounds().Dy()) / 2
+
+	draw.Draw(resizedImg, img.Bounds().Add(image.Pt(centerX, centerY)), img, image.Point{}, draw.Over)
+
+	return resizedImg
+}
+
+func saveImageToFile(img image.Image, file io.Writer) error {
+	return jpeg.Encode(file, img, nil)
 }
 
 // 指定したindexを削除したスライスを返す
@@ -155,21 +202,23 @@ func main() {
 	for json := range ch {
 		photoList = append(photoList, json.Photos.PhotoList...)
 	}
+	if len(photoList) == 0 {
+		log.Fatal("photo list is empty")
+	}
 	//fmt.Println(photoList)
 
-	var mu sync.Mutex
-	ctx, cancel = context.WithTimeout(context.Background(), TimeoutSeconds*time.Second)
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	for i := 1; i < 4; i++ {
 		wg.Add(1)
 		tmp := rand.Intn(len(photoList))
-		go func(photo []flickrapi.Photo, tmp int, idx int) {
+
+		go func(photo flickrapi.Photo, idx int) {
 			defer wg.Done()
 			errCnt := 0
-
 			for {
-				err := SavePhotoWithContext(ctx, conf, photoList[tmp], idx)
+				err := SavePhotoWithContext(ctx, conf, photo, idx)
 				if err != nil && errCnt < 3 {
 					errCnt++
 					if 3 <= errCnt {
@@ -177,13 +226,11 @@ func main() {
 						break
 					}
 				} else {
-					mu.Lock()
-					photoList = removeEl(photoList, tmp)
-					mu.Unlock()
 					break
 				}
 			}
-		}(photoList, tmp, i)
+		}(photoList[tmp], i)
+		photoList = removeEl(photoList, tmp)
 	}
 	wg.Wait()
 }
